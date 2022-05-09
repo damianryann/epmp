893f8937d4859076a8494813bbd61d6f379064e8
Damian.Ryan <Damian.Ryan@jacobs.com>
2022-05-09 11:20:08 +0100

    init

--- src/container/App/App.test.tsx
+++ src/container/App/App.test.tsx
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
+import React from 'react';
+
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Support title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Support/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/container/App/App.tsx
@@ -0,0 +1,149 @@
+import React from 'react';
+
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="container my-4 arrow">
+      <div className="row text-white">
+        <div className="col-md-12 col-lg-2 mt-1 mt-lg-1 mb-0 bg-blue-2 d-flex align-items-center justify-content-center">
+          <div className="h3 p-1 text-center fw-bold">Support</div>
+        </div>
+        <div className="p-0 col-md-12 col-lg-10 ps-lg-1">
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-2">
+            <div className="text-center">External Affairs &amp; Marketing</div>
+          </div>
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-2">
+            <div className="text-center">
+              Administrative, Welfare &amp; Facility Management Support
+            </div>
+          </div>
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-2">
+            <div className="text-center">
+              Innovation &amp; Continuous Improvement
+            </div>
+          </div>
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-2">
+            <div className="text-center">
+              Organisation, People &amp; Culture
+            </div>
+          </div>
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-2">
+            <div className="text-center">
+              Environment, Sustainability &amp; Carbon
+            </div>
+          </div>
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-2">
+            <div className="text-center">Health, Safety &amp; Wellbeing</div>
+          </div>
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-2">
+            <div className="text-center">
+              Communications &amp; Stakeholder Management
+            </div>
+          </div>
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-2">
+            <div className="text-center">Procurement</div>
+          </div>
+          <div className="mt-1 mt-md-1 mt-lg-1 mb-0 bg-blue-2">
+            <div className="text-center">
+              Technology, Digital Engineering &amp; IT/OT Security
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row text-white">
+        <div className="col-md-12 col-lg-2 mt-1 mt-lg-1 mb-0 bg-blue-4 d-flex align-items-center justify-content-center">
+          <div className="h3 text-center fw-bold">
+            Delivery &amp; Operations
+          </div>
+        </div>
+        <div className="col-md-12 col-lg-5 ps-0 ps-md-0 ps-lg-1 pe-0 pe-md-0 pe-lg-0">
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-4">
+            <div className="text-center">
+              Master &amp; Business Case Planning
+            </div>
+          </div>
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-4">
+            <div className="text-center">
+              Design &amp; Engineering Management
+            </div>
+          </div>
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-4">
+            <div className="text-center">Construction Management</div>
+          </div>
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-4">
+            <div className="text-center">Logistics and Security</div>
+          </div>
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-4">
+            <div className="text-center">
+              Systems Engineering &amp; Integration
+            </div>
+          </div>
+          <div className="mt-1 mt-md-1 mt-lg-1 mb-0 bg-blue-4">
+            <div className="text-center">Program Management</div>
+          </div>
+        </div>
+        <div className="col-md-12 col-lg-5 ps-0 ps-md-0 ps-lg-1 pe-0 pe-md-0 pe-lg-0">
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-4">
+            <div className="text-center">Interface Management</div>
+          </div>
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-4">
+            <div className="text-center">Supply Chain Management</div>
+          </div>
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-4">
+            <div className="text-center">
+              Operational Readiness &amp; Training
+            </div>
+          </div>
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-4">
+            <div className="text-center">
+              Permits, Consents Management &amp; Discharge
+            </div>
+          </div>
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-4">
+            <div className="text-center">Asset Management</div>
+          </div>
+          <div className="mt-1 mt-md-1 mt-lg-1 mb-0 bg-blue-4">
+            <div className="text-center">Maintenance</div>
+          </div>
+        </div>
+      </div>
+      <div className="row text-white">
+        <div className="col-md-12 col-lg-2 mt-1 mt-lg-1 mb-0 bg-blue-1 d-flex align-items-center justify-content-center">
+          <div className="h3 text-center fw-bold">Assurance</div>
+        </div>
+        <div className="p-0 col-md-12 col-lg-10 ps-lg-1">
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-1">
+            <div className="text-center">Governance &amp; Transformation</div>
+          </div>
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-1">
+            <div className="text-center">Quality Management</div>
+          </div>
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-1">
+            <div className="text-center">Risk Management</div>
+          </div>
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-1">
+            <div className="text-center">Finance &amp; Contract Management</div>
+          </div>
+          <div className="my-1 my-md-1 my-lg-1 bg-blue-1">
+            <div className="text-center">
+              Project Controls (Cost, Schedule, Change, Performance Mgmt &amp;
+              Analytics, Reporting, Document Mgmt)
+            </div>
+          </div>
+          <div className="mt-1 mt-md-1 mt-lg-1 mb-0 bg-blue-1">
+            <div className="text-center">Legacy &amp; Benefits</div>
+          </div>
+        </div>
+      </div>
+      <div className="left-side d-flex align-items-center justify-content-center">
+        <div className="h2">Outcomes</div>
+      </div>
+      <div className="right-side d-flex align-items-center justify-content-center">
+        <div className="h2">Outcomes</div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
